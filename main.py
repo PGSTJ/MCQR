9ed46802df14a607740c64d3fb89e2ca5504df11
Therry Malone <therrymalone@gmail.com>
2024-02-14 05:46:06 -0800

    sync - webpage wip

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+import os
+from flask import (Flask, redirect, render_template, request, send_file, session, url_for)
+
 import database
 
 # if database.create_all():
 #     print('done')
 
-def best_matches():
-    """
-    Finds best match of each submitted entry
 
-    database.find
+app = Flask(__name__)
 
-    """
-    all_matches = database.find_match()
-    e = [matches.submitter for matches in all_matches]
+@app.route('/')
+def home():
+    match_data = database.get_all_best_matches()
+    match_details = database.get_match_details(match_data)
+
+    return render_template('home.html', all_matches=match_data, m_details=match_details)
+
+
+# def all_best_matches():
+#     """
+#     Finds best match of each submitted entry
+
+#     database.find_match() returns a Comparison Report Object, you can access the following attributes:
+    
+#         .submitter
+#         .best_match
+#         .sub_data
+#         .best_match_data
+
+#     """
+#     all_matches = database.get_all_best_matches()
     
 
 if __name__ == '__main__':
-    matches()
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
